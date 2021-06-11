7c00c4c89ba260e4151fc1b43f65c1422d1b0c12
lz860704 <543972930@qq.com>
2021-06-11 20:41:19 +0800

    update code

--- Screen_recording.py
+++ Screen_recording.py
@@ -26,38 +26,35 @@ def get_desktop():
     img.save('D:\@@@\桌面截图_{}.jpg'.format(cur_time)) # 保存文件的名字
     # img.show()
 
-def get_recode_start():
+def get_recode():
 
     isExists=os.path.exists('D:\@@@')
     if not isExists:
         os.mkdir('D:\@@@')
     else:
         pass
-    p = ImageGrab.grab() #获得当前屏幕
-    k=np.zeros((9,9),np.uint8)
-    a,b=p.size#获得当前屏幕的大小
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')#编码格式
-    time_tup=time.localtime(time.time())
+    vie = ImageGrab.grab() #获得当前屏幕
+    width = vie.size[0]
+    height = vie.size[1]
+    k=np.zeros((1,1),np.uint8)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID') # 编码格式
+    time_tup = time.localtime(time.time())
     format_time="%Y%m%d%H%M%S"
     cur_time=time.strftime(format_time,time_tup)
-    video = cv2.VideoWriter('屏幕录制_{}.mp4'.format(cur_time), fourcc, 30, (a, b))#输出文件命名为test.mp4,帧率为16，可以自己设置
+    video = cv2.VideoWriter('D:\@@@\屏幕录制_{}.mp4'.format(cur_time), fourcc, 30, (width,height)) # 输出文件命名,帧率为30
     while True:
         im = ImageGrab.grab()
-        imm=cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)#转为opencv的BGR格式
+        imm = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR) # 转为opencv的BGR格式
         video.write(imm)
-        cv2.imshow('recoding', k)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-          break
+        cv2.imshow('imm', k)
+        if cv2.waitKey(1) & 0xFF == ord('a'):
+            break
     video.release()
     cv2.destroyAllWindows()
 
-def get_recode_stop():
-    pass
-
 
 layout = [
-    [sg.Button('录屏开始', key = '_VIDEOSTART_', font='微软雅黑', size=(9, 1)), sg.Button('录屏结束', key = '_VIDEOSTOP_', font='微软雅黑', size=(9, 1)), sg.Button('打开', key='_FOLDER_', size=(9, 1))],
-    [sg.Button('抓图', key = '_PHOTO_', font='微软雅黑', size=(9, 1)), sg.Button('打开', key='_FOLDER_', size=(9, 1)), sg.Exit('退出', key = '_EXIT_', font='微软雅黑', size=(9, 1))]
+    [sg.Button('抓图', key = '_PHOTO_', font='微软雅黑', size=(9, 1)), sg.Button('录屏', key = '_VIDEO_', font='微软雅黑', size=(9, 1)), sg.Button('打开', key='_FOLDER_', size=(9, 1)), sg.Exit('退出', key = '_EXIT_', font='微软雅黑', size=(9, 1))]
 ]
 # 定义窗口，窗口名称
 window = sg.Window('抓图录屏工具',layout,font='微软雅黑')
@@ -69,12 +66,10 @@ while True:
         get_desktop = get_desktop()
         window.reappear() # 显示窗口
         # sg.popup_ok('已下载至当前目录',title='成功',font='微软雅黑')
-    elif event == '_VIDEOSTART_':
+    elif event == '_VIDEO_':
         window.disappear() # 隐藏窗口
-        get_recode_start = get_recode_start()
+        get_recode = get_recode()
         window.reappear() # 显示窗口
-    elif event == '_VIDEOSTOP_':
-        get_recode_stop = get_recode_stop()
     elif event == '_FOLDER_':
         os.startfile(r"D:\@@@")
     elif event in ['_EXIT_',None]:
