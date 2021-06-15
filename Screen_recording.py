dc521c25169d07253e8283c259e5c6f62b402b61
lz860704 <543972930@qq.com>
2021-06-15 20:29:44 +0800

    update code

--- Screen_recording.py
+++ Screen_recording.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding:utf-8 -*-
 # Author:lz
 
@@ -25,6 +25,7 @@ def get_desktop():
     cur_time=time.strftime(format_time,time_tup)
     img.save('D:\@@@\桌面截图_{}.jpg'.format(cur_time)) # 保存文件的名字
     # img.show()
+
 
 def get_recode():
 
@@ -54,7 +55,7 @@ def get_recode():
 
 
 layout = [
-    [sg.Button('抓图', key = '_PHOTO_', font='微软雅黑', size=(9, 1)), sg.Button('录屏', key = '_VIDEO_', font='微软雅黑', size=(9, 1)), sg.Button('打开', key='_FOLDER_', size=(9, 1)), sg.Exit('退出', key = '_EXIT_', font='微软雅黑', size=(9, 1))]
+    [sg.Button('抓屏', key = '_PHOTO_', font='微软雅黑', size=(9, 1)), sg.Button('录屏', key = '_VIDEO_', font='微软雅黑', size=(9, 1)), sg.Button('打开', key='_FOLDER_', size=(9, 1)), sg.Exit('退出', key = '_EXIT_', font='微软雅黑', size=(9, 1))]
 ]
 # 定义窗口，窗口名称
 window = sg.Window('抓图录屏工具',layout,font='微软雅黑')
@@ -63,12 +64,12 @@ while True:
     event,values = window.read()
     if event == '_PHOTO_':
         window.disappear() # 隐藏窗口
-        get_desktop = get_desktop()
+        get_desktop()
         window.reappear() # 显示窗口
         # sg.popup_ok('已下载至当前目录',title='成功',font='微软雅黑')
     elif event == '_VIDEO_':
         window.disappear() # 隐藏窗口
-        get_recode = get_recode()
+        get_recode()
         window.reappear() # 显示窗口
     elif event == '_FOLDER_':
         os.startfile(r"D:\@@@")
