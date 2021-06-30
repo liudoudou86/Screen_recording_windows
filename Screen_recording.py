1dced0e4091fe097e3414f3a52f07f0cc77f9b23
lz860704 <543972930@qq.com>
2021-06-30 17:09:24 +0800

    update code

--- Screen_recording.py
+++ Screen_recording.py
@@ -3,7 +3,6 @@
 # Author:lz
 
 
-import ctypes
 import os
 import time
 import tkinter
@@ -12,6 +11,8 @@ import cv2
 import numpy as np
 import PySimpleGUI as sg
 from PIL import ImageGrab
+from pynput import keyboard
+import threading
 
 
 class mouse_screen:
@@ -23,61 +24,49 @@ class mouse_screen:
         pass
 
     def __init__(self):
-        self.__start_x, self.__start_y = 0, 0
-        self.__scale = 1
+        self.start_x, self.start_y = 0, 0
+        self.scale = 1
 
-        self.__win = tkinter.Tk()
-        self.__win.attributes("-alpha", 0.1)  # 设置窗口半透明
-        self.__win.attributes("-fullscreen", True)  # 设置全屏
-        self.__win.attributes("-topmost", True)  # 设置窗口在最上层
+        self.win = tkinter.Tk()
+        self.win.attributes("-alpha", 0.2)  # 设置窗口半透明
+        self.win.attributes("-fullscreen", True)  # 设置全屏
+        self.win.attributes("-topmost", True)  # 设置窗口在最上层
 
-        self.__width, self.__height = self.__win.winfo_screenwidth(), self.__win.winfo_screenheight()
+        self.width, self.height = self.win.winfo_screenwidth(), self.win.winfo_screenheight()
 
         # 创建画布
-        self.__canvas = tkinter.Canvas(self.__win, width=self.__width, height=self.__height, bg="gray")
+        self.canvas = tkinter.Canvas(self.win, width=self.width, height=self.height, bg="black")
 
-        self.__win.bind('<Button-1>', self.xFunc1)  # 绑定鼠标左键点击事件
-        self.__win.bind('<ButtonRelease-1>', self.xFunc1)  # 绑定鼠标左键点击释放事件
-        self.__win.bind('<B1-Motion>', self.xFunc2)  # 绑定鼠标左键点击移动事件
-        self.__win.bind('<Escape>', lambda e: self.__win.destroy())  # 绑定Esc按键退出事件
-        '''
-        user32 = ctypes.windll.user32
-        gdi32 = ctypes.windll.gdi32
-        dc = user32.GetDC(None)
-        widthScale = gdi32.GetDeviceCaps(dc, 8)  # 分辨率缩放后的宽度
-        heightScale = gdi32.GetDeviceCaps(dc, 10)  # 分辨率缩放后的高度
-        width = gdi32.GetDeviceCaps(dc, 118)  # 原始分辨率的宽度
-        height = gdi32.GetDeviceCaps(dc, 117)  # 原始分辨率的高度
-        self.__scale = width / widthScale
-        # print(self.__width, self.__height, widthScale, heightScale, width, height, self.__scale)
-        # self.__win.mainloop()  # 窗口持久化
-        '''
-    def xFunc1(self, event):
+        self.win.bind('<Button-1>', self.Fun1)  # 绑定鼠标左键点击事件
+        self.win.bind('<ButtonRelease-1>', self.Fun1)  # 绑定鼠标左键点击释放事件
+        self.win.bind('<B1-Motion>', self.Fun2)  # 绑定鼠标左键点击移动事件
+        self.win.bind('<Escape>', lambda e: self.win.destroy())  # 绑定Esc按键退出事件
+
+    def Fun1(self, event):
         # print(f"鼠标左键点击了一次坐标是:x={g_scale * event.x}, y={g_scale * event.y}")
         if event.state == 8:  # 鼠标左键按下
-            self.__start_x, self.__start_y = event.x, event.y
+            self.start_x, self.start_y = event.x, event.y
         elif event.state == 264:  # 鼠标左键释放
-            if event.x == self.__start_x or event.y == self.__start_y:
+            if event.x == self.start_x or event.y == self.start_y:
                 return
-            img = ImageGrab.grab((self.__scale * self.__start_x, self.__scale * self.__start_y, self.__scale * event.x, self.__scale * event.y))
+            img = ImageGrab.grab((self.scale * self.start_x, self.scale * self.start_y, self.scale * event.x, self.scale * event.y))
             time_tup=time.localtime(time.time()) # 获取当前时间
             format_time="%Y-%m-%d_%H-%M-%S"
             cur_time=time.strftime(format_time,time_tup)
-            img.save('D:\@@@\Screen_{}.png'.format(cur_time)) # 保存文件的名字
+            img.save('D:\@@@\Screenshots_{}.png'.format(cur_time)) # 保存文件的名字
 
-            self.__win.update()
+            self.win.update()
             time.sleep(0.5)
-            self.__win.destroy()
+            self.win.destroy()
 
-    def xFunc2(self, event):
+    def Fun2(self, event):
         # print(f"鼠标左键点击了一次坐标是:x={self.__scale * event.x}, y={self.__scale * event.y}")
-        if event.x == self.__start_x or event.y == self.__start_y:
+        if event.x == self.start_x or event.y == self.start_y:
             return
-        self.__canvas.delete("prscrn")
-        self.__canvas.create_rectangle(self.__start_x, self.__start_y, event.x, event.y,
-                                       fill='white', outline='red', tag="prscrn")
+        self.canvas.delete()
+        self.canvas.create_rectangle(self.start_x, self.start_y, event.x, event.y, fill='white', outline='red')
         # 包装画布
-        self.__canvas.pack()
+        self.canvas.pack()
 
 def get_desktop():
 
@@ -90,9 +79,11 @@ def get_desktop():
     format_time="%Y-%m-%d_%H-%M-%S"
     cur_time=time.strftime(format_time,time_tup)
     img = ImageGrab.grab() # 获取当前屏幕内容
-    img.save('D:\@@@\Screen_{}.png'.format(cur_time)) # 保存文件的名字
+    img.save('D:\@@@\Screenshots_{}.png'.format(cur_time)) # 保存文件的名字
     # img.show()
 
+
+flag=False  #停止标志位
 
 def get_recode():
 
@@ -104,22 +95,26 @@ def get_recode():
     vie = ImageGrab.grab() #获得当前屏幕
     width = vie.size[0]
     height = vie.size[1]
-    k=np.zeros((1,1),np.uint8)
     time_tup = time.localtime(time.time())
     format_time="%Y-%m-%d_%H-%M-%S"
     cur_time=time.strftime(format_time,time_tup)
-    fourcc = cv2.VideoWriter_fourcc(*'X264') # 规定编码器编码视频格式
-    video = cv2.VideoWriter('D:\@@@\Record_{}.mp4'.format(cur_time), fourcc, 60, (width,height)) # 输出文件命名,帧率为30
+    fourcc = cv2.VideoWriter_fourcc(*'XVID') # 规定编码器编码视频格式
+    video = cv2.VideoWriter('D:\@@@\Record_{}.avi'.format(cur_time), fourcc, 60, (width,height)) # 输出文件命名,帧率为30
     while True:
         im = ImageGrab.grab()
         imm = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR) # 转为opencv的BGR格式
         video.write(imm)
-        cv2.imshow('imm', k)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            print(ord('q'))
+        if flag:
             break
     video.release()
-    cv2.destroyAllWindows()
+
+
+def on_press(key):
+
+    global flag
+    if key == keyboard.Key.esc:
+        flag=True
+        return False  #返回False，键盘监听结束！
 
 
 layout = [
@@ -131,17 +126,16 @@ window = sg.Window('截图录屏工具',layout,font='微软雅黑')
 while True:
     event,values = window.read()
     if event == '_MOUSE_':
-        window.disappear() # 隐藏窗口
         mouse_screen()
-        window.reappear() # 显示窗口
     elif event == '_PHOTO_':
         window.disappear() # 隐藏窗口
         get_desktop()
         window.reappear() # 显示窗口
     elif event == '_VIDEO_':
-        window.disappear() # 隐藏窗口
-        get_recode()
-        window.reappear() # 显示窗口
+        th=threading.Thread(target=get_recode)
+        th.start()
+        with keyboard.Listener(on_press=on_press) as listener:
+            listener.join()
     elif event == '_FOLDER_':
         os.startfile(r"D:\@@@")
     elif event in ['_EXIT_',None]:
